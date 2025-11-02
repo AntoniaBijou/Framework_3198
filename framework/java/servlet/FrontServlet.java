0592e3a4dbc9196adcabe0de94a1d2c6095260d9
Antonia Bijou <163192297+AntoniaBijou@users.noreply.github.com>
2025-11-02 20:03:18 +0300

    sprint1-bis

--- framework/java/servlet/FrontServlet.java
+++ framework/java/servlet/FrontServlet.java
@@ -14,7 +14,8 @@ import jakarta.servlet.http.HttpServletResponse;
 public class FrontServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        chercherRessource(req, resp);
+        // chercherRessource(req, resp);
+        afficherRessource(req, resp);
     }
 
     private void chercherRessource(HttpServletRequest req, HttpServletResponse resp)
@@ -35,45 +36,44 @@ public class FrontServlet extends HttpServlet {
         }
     }
 
-    // private void chercherRessource(HttpServletRequest req, HttpServletResponse
-    // resp)
-    // throws ServletException, IOException {
+    private void afficherRessource(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
 
-    // String path = req.getRequestURI().substring(req.getContextPath().length());
+        String path = req.getRequestURI().substring(req.getContextPath().length());
 
-    // if ("/".equals(path)) {
-    // resp.getWriter().println("/");
-    // return;
-    // }
+        if ("/".equals(path)) {
+            resp.getWriter().println("/");
+            return;
+        }
 
-    // if (path.startsWith("/")) {
-    // path = path.substring(1);
-    // }
-    // String webInfPath = "/WEB-INF/views/";
-    // String viewPath;
-    // if (path.startsWith("WEB-INF/views/")) {
-    // viewPath = "/" + path;
-    // } else {
-    // viewPath = webInfPath + path;
-    // }
+        if (path.startsWith("/")) {
+            path = path.substring(1);
+        }
+        String webInfPath = "/WEB-INF/views/";
+        String viewPath;
+        if (path.startsWith("WEB-INF/views/")) {
+            viewPath = "/" + path;
+        } else {
+            viewPath = webInfPath + path;
+        }
 
-    // File file = new File(getServletContext().getRealPath(viewPath));
+        File file = new File(getServletContext().getRealPath(viewPath));
 
-    // if (file.exists() && file.isFile()) {
-    // resp.setContentType("text/html; charset=UTF-8");
-    // try (FileInputStream fis = new FileInputStream(file);
-    // OutputStream os = resp.getOutputStream()) {
+        if (file.exists() && file.isFile()) {
+            resp.setContentType("text/html; charset=UTF-8");
+            try (FileInputStream fis = new FileInputStream(file);
+                    OutputStream os = resp.getOutputStream()) {
 
-    // byte[] buffer = new byte[4096];
-    // int bytesRead;
-    // while ((bytesRead = fis.read(buffer)) != -1) {
-    // os.write(buffer, 0, bytesRead);
-    // }
-    // }
-    // } else {
-    // resp.setContentType("text/html; charset=UTF-8");
-    // resp.getWriter().println("<h3>Page introuvable : " + path + "</h3>");
-    // }
-    // }
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+                while ((bytesRead = fis.read(buffer)) != -1) {
+                    os.write(buffer, 0, bytesRead);
+                }
+            }
+        } else {
+            resp.setContentType("text/html; charset=UTF-8");
+            resp.getWriter().println("<h3>Page introuvable : " + path + "</h3>");
+        }
+    }
 
 }
